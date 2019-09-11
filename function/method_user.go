bc63d81830be1ae54ea8f296d361dc5dc30fc118
tosaharuki <13haruki28@gmail.com>
2019-09-11 12:51:31 +0900

    ##

--- function/method_user.go
+++ function/method_user.go
@@ -10,8 +10,8 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
-	_ "github.com/mattn/go-sqlite3"
-    _ "github.com/lib/pq"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
 )
 
 var DatabaseName string
--- go.mod
+++ go.mod
@@ -8,7 +8,6 @@ require (
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.3
 	github.com/jinzhu/gorm v1.9.10
-	github.com/lib/pq v1.1.1
 	github.com/mattn/go-sqlite3 v1.11.0
 	google.golang.org/api v0.9.0
 )
